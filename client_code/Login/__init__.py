77d889ec0ab793ac9aa74f5b8f43fa9d21a26d98
avfscnsp@gmail.com <avfscnsp@gmail.com>
2023-11-15 19:41:21 +0000

    Edited settings and Login

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -12,4 +12,25 @@ class Login(LoginTemplate):
     self.init_components(**properties)
     anvil.users.login_with_form()
     print(f"This user has logged in: {anvil.users.get_user()['email']}")
+    alert('Logou como: '+anvil.users.get_user()['email'])
     # Any code you write here will run before the form opens.
+
+  def label_1_show(self, **event_args):
+    """This method is called when the Label is shown on the screen"""
+    self.label_1.text = ('logado como: ' + anvil.users.get_user()['email'])
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.users.logout()
+    alert('Deslogou')
+    anvil.users.login_with_form()
+
+  def PUBLICAR_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    app_tables.feedbacks.add_row(Usuario = anvil.users.get_user()['email'],
+                          Feedback = self.FEEDBACK.text)
+    
+    
+    
+    
+
