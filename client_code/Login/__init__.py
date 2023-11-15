52c8ef52a4dece6774e1410777560c5523e7a762
avfscnsp@gmail.com <avfscnsp@gmail.com>
2023-11-15 18:45:39 +0000

    Created Login; Edited settings and ServerModule1; Deleted Form1

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -1,13 +1,15 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import LoginTemplate
 from anvil import *
+import anvil.users
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class Login(LoginTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    anvil.users.login_with_form()
+    print(f"This user has logged in: {anvil.users.get_user()['email']}")
     # Any code you write here will run before the form opens.
