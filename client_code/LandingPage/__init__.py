59f071748332acff727885983c301b230c560645
avfscnsp@gmail.com <avfscnsp@gmail.com>
2024-03-15 23:13:55 +0000

    Edited LandingPage

--- client_code/LandingPage/__init__.py
+++ client_code/LandingPage/__init__.py
@@ -12,3 +12,6 @@ class LandingPage(LandingPageTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+def click_login_button(self):
+  pass
